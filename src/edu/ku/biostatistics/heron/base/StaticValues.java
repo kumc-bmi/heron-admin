bb66056bc6ebe59ca5164dd46973c320edc7c7ca
Dongsheng zhu <dzhu@kumc.edu>
2011-01-05 14:29:17 -0600

    dev

--- src/edu/ku/biostatistics/heron/base/StaticValues.java
+++ src/edu/ku/biostatistics/heron/base/StaticValues.java
@@ -4,4 +4,9 @@ public interface StaticValues {
 	String CHALK_URL = "chalk_url";
 	String CAS_LOGIN_URL = "cas_login_url";
 	String I2B2_CLIENT_SERVICE = "i2b2_client_service";
+	String USER_NAME = "user_name";
+	String NOT_QUALIFIED_URL = "not_qualified.jsp";
+	String NOT_TRAINED_URL = "not_trained.jsp";
+	String SAA_URL = "system_access_agreement.jsp";
+	String DENIED_URL = "http://www.kumc.edu/";
 }
