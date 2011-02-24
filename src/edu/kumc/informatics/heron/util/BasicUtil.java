425079a756a615c9aa647e26693a2bd75cfa2ec2
Dongsheng zhu <dzhu@kumc.edu>
2011-02-24 10:52:41 -0600

    change for email notification of request approval/reject

--- src/edu/kumc/informatics/heron/util/BasicUtil.java
+++ src/edu/kumc/informatics/heron/util/BasicUtil.java
@@ -1,5 +1,7 @@
 package edu.kumc.informatics.heron.util;
 
+import static edu.kumc.informatics.heron.base.StaticValues.RAVEN_URL;
+
 import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,6 +20,7 @@ import javax.mail.internet.MimeMessage;
  */
 public class BasicUtil {
 	private LdapUtil ldapUtil = new LdapUtil();
+	private Properties props = StaticDataUtil.getSoleInstance().getProperties();
 	/**
 	 * check if a date string is valid
 	 * @param aDate
@@ -78,17 +81,20 @@ public class BasicUtil {
 	 * @param subj
 	 * @param contn
 	 * @param host
+	 * @param ccEmails
 	 */
-	public void sendEmails(String fromAddr, String toEmails, String subj, String contn,String host){
+	public void sendEmails(String fromAddr, String toEmails, String subj, String contn,String host,String ccEmails){
 		try {
 		      Properties props = new Properties();
 		      props.put("mail.smtp.host", host);
 		      Session mailConnection = Session.getInstance(props,  null);
 		      Message msg = new MimeMessage(mailConnection);
 		      Address[] addrs = InternetAddress.parse(toEmails);
+		      Address[] ccAddrs = InternetAddress.parse(ccEmails);
 		      msg.setContent(contn, "text/plain");
 		      msg.setFrom(new InternetAddress(fromAddr));
 		      msg.setRecipients(Message.RecipientType.TO, addrs);
+		      msg.setRecipients(Message.RecipientType.CC,ccAddrs);
 		      msg.setSubject(subj);
 		      Transport.send(msg);  
 		}
@@ -109,6 +115,32 @@ public class BasicUtil {
 			" and follow the \"Approve Sponsored HERON Users\" link \n\n"+
 			"Sincerely, \n \n"+
 			"The HERON Team.";
-		this.sendEmails("heron-admin@kumc.edu", toEmails, subj, contn, "smtp.kumc.edu");
+		this.sendEmails("heron-admin@kumc.edu", toEmails, subj, contn, "smtp.kumc.edu","");
+	}
+	
+	/**
+	 * send notification emails to User about sponsorship Approval Or Rejection
+	 * @param userId
+	 * @param sponsorId
+	 * @param type
+	 */
+	public void notifyUserApprovedOrRejected(String userId,String sponsorId, String type){
+		String userEmail = ldapUtil.getLdapAttributeByName(userId,"mail");
+		String spnEmail = ldapUtil.getLdapAttributeByName(sponsorId,"mail");
+		String subj = "HERON Request is ";
+		String contn = "Dear HERON User,\n \n "+ "The user id ("+ userId+ ") you sponsored has been ";
+		
+		if(type.equals("A")){
+			subj += "approved!";
+			contn += "approved by the DROC team. \n \n" +
+				" The approved user can visit: \n\n" + props.getProperty(RAVEN_URL) +
+				" and follow the \"HERON\" link  to use the application.\n\n";
+		}
+		else{
+			subj += "rejected!";
+			contn += "rejected by a committee member of the DROC team. \n \n";
+		}
+		contn += "Sincerely, \n \n"+ "The HERON Team.";
+		this.sendEmails("heron-admin@kumc.edu", spnEmail, subj, contn, "smtp.kumc.edu",userEmail);
 	}
 }
