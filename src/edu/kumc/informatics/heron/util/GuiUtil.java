09c54040d34990f8433bf3442b5ad49fa08b2f63
Dongsheng zhu <dzhu@kumc.edu>
2011-03-21 12:59:53 -0500

    ticket 354

--- src/edu/kumc/informatics/heron/util/GuiUtil.java
+++ src/edu/kumc/informatics/heron/util/GuiUtil.java
@@ -5,12 +5,19 @@ import java.util.List;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.commons.collections.map.ListOrderedMap;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import edu.harvard.i2b2.common.util.xml.XMLUtil;
 import edu.kumc.informatics.heron.dao.HeronDBDao;
 import edu.kumc.informatics.heron.dao.HeronReportsDao;
 
 /**
  * build gui components. 
- * 
+ * intended for simple gui build.
+ * //TODO:as application getting more complicated, need to switch to better gui framework(struts/jsf)
  * @author dzhu
  *
  */
@@ -121,17 +128,28 @@ public class GuiUtil {
 			if(usersList==null || usersList.size()==0)
 				return "<div align=center>There is no users at this time.</div>";
 			bf.append("<div align=center>Total Users Count: "+usersList.size()+"</div>");
-			bf.append("<div align=center><table class=\"heron\"><tr><th>User Id</th><th>User Name</th></tr>");
+			bf.append("<div align=center><table class=\"heron\"><tr><th>User_Id</th><th>User_Name</th>");
+			bf.append("<th>Logon_Count</th><th>Query_Count</th></tr>");
 			
 			for(int i=0;i<usersList.size();i++){
 				Object aMap = usersList.get(i);
 				String userId = ((ListOrderedMap)aMap).get("USER_ID")+"";
 				String name = ((ListOrderedMap)aMap).get("user_full_name")+"";
+				String sCount = ((ListOrderedMap)aMap).get("session_count")+"";
+				String qCount = ((ListOrderedMap)aMap).get("query_count")+"";
+	
 				String rowStyle = i%2==0?"<tr class=\"d0\"><td>":"<tr class=\"d1\"><td>";
 				bf.append(rowStyle);
 				bf.append(userId);
 				bf.append("</td><td>");
 				bf.append(name);
+				bf.append("</td><td>");
+				bf.append(sCount);
+				bf.append("</td><td>");
+				if(!qCount.equals("0"))
+					bf.append("<a href=\"query_report_details.jsp?uid="+userId+"\">" + qCount + "</a>");
+				else 
+					bf.append(qCount);
 				bf.append("</td></tr>");
 			}
 			bf.append("</table></div>");
@@ -197,5 +215,75 @@ public class GuiUtil {
 	 */
 	public String getRecentDisclaimer(){
 		return heronDao.getRecentDisclaimer();
+	}
+	
+	/**
+	 * build user query report
+	 * @param userId
+	 * @return string/html
+	 */
+	public String getQueryReport(String userId){
+		List aList = rptDao.getQueryReportInfo(userId);
+		StringBuffer bf = new StringBuffer("");
+		bf.append("<div align=center>Query Report for User: "+userId+"</div>");
+		bf.append("<div align=center><table class=\"heron\"><tr><th>Query_Name</th><th>Query_Details</th>");
+		bf.append("<th>Start</th><th>Set_Size</th><th>Real_Set_Size</th><th>Description</th></tr>");
+
+		try{
+			for(int i=0;i<aList.size();i++){
+				Object aMap = aList.get(i);
+				String qname = ((ListOrderedMap)aMap).get("name")+"";
+				String qxml = ((ListOrderedMap)aMap).get("request_xml")+"";
+				String sSize = ((ListOrderedMap)aMap).get("set_size")+"";
+				String sDate = ((ListOrderedMap)aMap).get("start_date")+"";
+				String rSize = ((ListOrderedMap)aMap).get("real_set_size")+"";
+				String description = ((ListOrderedMap)aMap).get("description")+"";
+				String rowStyle = i%2==0?"<tr class=\"d0\"><td>":"<tr class=\"d1\"><td>";
+				Document doc = XMLUtil.convertStringToDOM(qxml);
+				NodeList nList = doc.getElementsByTagName("panel");
+				
+				bf.append(rowStyle);
+				bf.append(qname);
+				bf.append("</td><td>");
+				
+				for(int j=0;j<nList.getLength();j++){
+					bf.append("Panel "+j+": <br>");
+					Node node = nList.item(j);
+					NodeList nlist2 = node.getChildNodes();
+					for(int k=0;k<nlist2.getLength();k++){
+						Node node2 = nlist2.item(k);
+						if(node2.getNodeName().equals("item")){
+							NodeList nList3 = node2.getChildNodes();
+							for(int m=0;m<nList3.getLength();m++){
+								Node node3 = nList3.item(m);
+								if(node3.getNodeName().equals("item_name")){
+									bf.append("item_name: "+node3.getFirstChild().getNodeValue()+";");
+								}
+								else if(node3.getNodeName().equals("tooltip")){
+									bf.append("tooltip: "+node3.getFirstChild().getNodeValue()+";<br>");
+								}
+									
+							}
+						}
+							
+					}
+				}
+	
+				bf.append("</td><td>");
+				bf.append(sDate);
+				bf.append("</td><td>");
+				bf.append(sSize);
+				bf.append("</td><td>");
+				bf.append(rSize);
+				bf.append("</td><td>");
+				bf.append(description.equals("null")?"":description);
+				bf.append("</td></tr>");
+			}
+			bf.append("</table></div>");
+			return bf.toString();
+		}
+		catch(Exception ex){
+			return "Error parsing request xml";
+		}
 	}
 }
