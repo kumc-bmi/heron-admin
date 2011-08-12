71f532abd73368a135a1030db923d8c179a6de5d
Dan Connolly <http://www.madmode.com/>
2011-08-11 23:45:12 -0500

    reformat a couple source files per eclipse norms
    --HG--
    branch : frontiers-layout

--- src/main/java/edu/kumc/informatics/heron/capsec/SystemAccessRecords.java
+++ src/main/java/edu/kumc/informatics/heron/capsec/SystemAccessRecords.java
@@ -7,27 +7,30 @@ import javax.naming.NameNotFoundException;
 import javax.naming.NoPermissionException;
 
 /**
- *
+ * 
  * @author dconnolly
  */
 public interface SystemAccessRecords {
-        /**
-         * Turn a Ticket into a RepositoryUser, provided the ticket is from
-         * a recognized agent who signed the system access agreement.
-         * @param who
-         * @return
-         * @throws NoPermissionException
-         */
-	public RepositoryUser asUser(Ticket who) throws NoPermissionException, NameNotFoundException;
-        
-        /**
-         * Turn a Ticket into a Sponsor, provided the ticket is from
-         * a qualified faculty member who signed the system access agreement.
-         * @param who
-         * @return
-         * @throws NoPermissionException
-         */
-        Sponsor asSponsor(Ticket who) throws NoPermissionException;
+	/**
+	 * Turn a Ticket into a RepositoryUser, provided the ticket is from a
+	 * recognized agent who signed the system access agreement.
+	 * 
+	 * @param who
+	 * @return
+	 * @throws NoPermissionException
+	 */
+	public RepositoryUser asUser(Ticket who) throws NoPermissionException,
+			NameNotFoundException;
 
-        public static final String beanName = "sar";
+	/**
+	 * Turn a Ticket into a Sponsor, provided the ticket is from a qualified
+	 * faculty member who signed the system access agreement.
+	 * 
+	 * @param who
+	 * @return
+	 * @throws NoPermissionException
+	 */
+	Sponsor asSponsor(Ticket who) throws NoPermissionException;
+
+	public static final String beanName = "sar";
 }
--- src/test/java/edu/kumc/informatics/heron/capsec/LDAPEnterpriseTest.java
+++ src/test/java/edu/kumc/informatics/heron/capsec/LDAPEnterpriseTest.java
@@ -30,154 +30,168 @@ import edu.kumc.informatics.heron.util.HCardContext;
 import org.springframework.mock.web.MockHttpServletRequest;
 
 public class LDAPEnterpriseTest {
-        public static final String mockData = "/mockDirectory.html";
-            LdapTemplate cardsTemplate = new LdapTemplate(new HCardSource());
+	public static final String mockData = "/mockDirectory.html";
+	LdapTemplate cardsTemplate = new LdapTemplate(new HCardSource());
 
-        @Test
-        public void findKnownAgent() throws NameNotFoundException {
-            LDAPEnterprise e = new LDAPEnterprise(cardsTemplate);
-            Assert.assertEquals("John Smith", e.affiliate("john.smith").getFullName());
-        }
+	@Test
+	public void findKnownAgent() throws NameNotFoundException {
+		LDAPEnterprise e = new LDAPEnterprise(cardsTemplate);
+		Assert.assertEquals("John Smith", e.affiliate("john.smith")
+				.getFullName());
+	}
 
-        @Test(expected= NameNotFoundException.class)
-        public void dontFindUnknownAgent() throws NameNotFoundException {
-            LDAPEnterprise e = new LDAPEnterprise(cardsTemplate);
-            e.affiliate("nobody.we.know");
-        }
+	@Test(expected = NameNotFoundException.class)
+	public void dontFindUnknownAgent() throws NameNotFoundException {
+		LDAPEnterprise e = new LDAPEnterprise(cardsTemplate);
+		e.affiliate("nobody.we.know");
+	}
 
-        @Test
-        public void findFaculty() throws NoPermissionException, SecurityException{
-            LDAPEnterprise e = new LDAPEnterprise(cardsTemplate);
-            Agent fac = e.qualifiedFaculty(e.asTicket(mockCASRequest("john.smith")));
-            Assert.assertEquals("John Smith", fac.getFullName());
-        }
-        @Test(expected= NoPermissionException.class)
-        public void studentNotFaculty() throws NoPermissionException, SecurityException{
-            LDAPEnterprise e = new LDAPEnterprise(cardsTemplate);
-            e.qualifiedFaculty(e.asTicket(mockCASRequest("bill.student")));
-        }
+	@Test
+	public void findFaculty() throws NoPermissionException, SecurityException {
+		LDAPEnterprise e = new LDAPEnterprise(cardsTemplate);
+		Agent fac = e
+				.qualifiedFaculty(e.asTicket(mockCASRequest("john.smith")));
+		Assert.assertEquals("John Smith", fac.getFullName());
+	}
 
-        public static HttpServletRequest mockCASRequest(String userid) {
-            MockHttpServletRequest q = new MockHttpServletRequest("GET", "/");
-            Assertion a = new MockAssertion(userid);
-            q.getSession(true).setAttribute(AbstractCasFilter.CONST_CAS_ASSERTION, a);
-        	return q;
-        }
-        private static class MockAssertion implements Assertion {
+	@Test(expected = NoPermissionException.class)
+	public void studentNotFaculty() throws NoPermissionException,
+			SecurityException {
+		LDAPEnterprise e = new LDAPEnterprise(cardsTemplate);
+		e.qualifiedFaculty(e.asTicket(mockCASRequest("bill.student")));
+	}
+
+	public static HttpServletRequest mockCASRequest(String userid) {
+		MockHttpServletRequest q = new MockHttpServletRequest("GET", "/");
+		Assertion a = new MockAssertion(userid);
+		q.getSession(true).setAttribute(AbstractCasFilter.CONST_CAS_ASSERTION,
+				a);
+		return q;
+	}
+
+	private static class MockAssertion implements Assertion {
+		private static final long serialVersionUID = 1L;
+
+		private final MockPrincipal _who;
+
+		public MockAssertion(String name) {
+			_who = new MockPrincipal(name);
+		}
+
+		@Override
+		public Map<String, Object> getAttributes() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public AttributePrincipal getPrincipal() {
+			return _who;
+		}
+
+		@Override
+		public Date getValidFromDate() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public Date getValidUntilDate() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+	}
+
+	private static class MockPrincipal implements AttributePrincipal {
+		private static final long serialVersionUID = 1L;
+		private final String _name;
+
+		public MockPrincipal(String name) {
+			_name = name;
+		}
+
+		@Override
+		public Map<String, Object> getAttributes() {
+			return new HashMap<String, Object>();
+		}
+
+		@Override
+		public String getProxyTicketFor(String arg0) {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public String getName() {
+			return _name;
+		}
+
+	}
+
+	public static class KUMCHCardContext extends HCardContext {
+		public KUMCHCardContext(InputSource hCardSrc) {
+			super(hCardSrc);
+		}
+
+		private static final Map<String, String> ldap2hcard = new HashMap<String, String>() {
 			private static final long serialVersionUID = 1L;
 
-			private final MockPrincipal _who;
-        	public MockAssertion(String name) {
-        		_who = new MockPrincipal(name);
-        	}
-			@Override
-			public Map<String, Object> getAttributes() {
-				// TODO Auto-generated method stub
-				return null;
+			{
+				put("sn", "family-name");
+				put("givenname", "given-name");
+				put("mail", "email");
+				put("kumcPersonFaculty", "kumcPersonFaculty");
+				put("kumcPersonJobcode", "kumcPersonJobcode");
 			}
+		};
 
-			@Override
-			public AttributePrincipal getPrincipal() {
-				return _who;
+		public Attributes cardAttributes(Node hcard) {
+			BasicAttributes out = new BasicAttributes();
+			for (String ldapattr : ldap2hcard.keySet()) {
+				String hcardClass = ldap2hcard.get(ldapattr);
+				String findByClass = ".//*[@class='" + hcardClass + "']/text()";
+				String value;
+				try {
+					value = xpath.evaluate(findByClass, hcard);
+				} catch (XPathExpressionException ex) {
+					throw new RuntimeException(
+							"Bad XPath defined at design time:" + findByClass,
+							ex);
+				}
+				out.put(ldapattr, value);
 			}
+			return out;
+		}
+	}
 
-			@Override
-			public Date getValidFromDate() {
-				// TODO Auto-generated method stub
-				return null;
-			}
+	/**
+	 * Spring ContextSource to build a KUMCHCardContext
+	 */
+	public static class HCardSource implements ContextSource {
 
-			@Override
-			public Date getValidUntilDate() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-        	
-        }
-        
-        private static class MockPrincipal implements AttributePrincipal {
-			private static final long serialVersionUID = 1L;
-			private final String _name;
-        	public MockPrincipal(String name) {
-        		_name = name;
-        	}
+		@Override
+		public DirContext getReadOnlyContext() throws NamingException {
+			InputSource src = new InputSource(getClass().getResourceAsStream(
+					mockData));
+			return new KUMCHCardContext(src);
+		}
 
-        	@Override
-			public Map<String, Object> getAttributes() {
-				return new HashMap<String, Object>();
-			}
+		@Override
+		public DirContext getReadWriteContext() throws NamingException {
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
 
-			@Override
-			public String getProxyTicketFor(String arg0) {
-				throw new UnsupportedOperationException();
-			}
-
-			@Override
-			public String getName() {
-				return _name;
-			}
-        	
-        }
-
-        public static class KUMCHCardContext extends HCardContext {
-            public KUMCHCardContext(InputSource hCardSrc){
-                super(hCardSrc);
-            }
-
-            private static final Map<String, String> ldap2hcard = new HashMap<String, String>() {
-                {
-                    put("sn", "family-name");
-                    put("givenname", "given-name");
-                    put("mail", "email");
-                    put("kumcPersonFaculty", "kumcPersonFaculty");
-                    put("kumcPersonJobcode", "kumcPersonJobcode");
-                }
-            };
-            public Attributes cardAttributes(Node hcard) {
-                BasicAttributes out = new BasicAttributes();
-                for (String ldapattr : ldap2hcard.keySet()) {
-                    String hcardClass = ldap2hcard.get(ldapattr);
-                    String findByClass = ".//*[@class='" + hcardClass + "']/text()";
-                    String value;
-                    try {
-                        value = xpath.evaluate(findByClass, hcard);
-                    } catch (XPathExpressionException ex) {
-                        throw new RuntimeException("Bad XPath defined at design time:" + findByClass, ex);
-                    }
-                    out.put(ldapattr, value);    
-                }
-                return out;
-            }
-        }
-
-        /**
-         * Spring ContextSource to build a KUMCHCardContext
-         */
-        public static class HCardSource implements ContextSource {
-            
-            @Override
-            public DirContext getReadOnlyContext() throws NamingException {
-                InputSource src =
-                        new InputSource(getClass().getResourceAsStream(mockData));
-                return new KUMCHCardContext(src);
-            }
-            
-            @Override
-            public DirContext getReadWriteContext() throws NamingException {
-                throw new UnsupportedOperationException("Not supported yet.");
-            }
-            
-            /**
-             * I'm not sure this is faithful to the interface...
-             * @param u
-             * @param p
-             * @return
-             * @throws NamingException 
-             */
-            @Override
-            public DirContext getContext(String u, String p) throws NamingException {
-                return getReadOnlyContext();
-            }
-        }        
+		/**
+		 * I'm not sure this is faithful to the interface...
+		 * 
+		 * @param u
+		 * @param p
+		 * @return
+		 * @throws NamingException
+		 */
+		@Override
+		public DirContext getContext(String u, String p) throws NamingException {
+			return getReadOnlyContext();
+		}
+	}
 }
-
