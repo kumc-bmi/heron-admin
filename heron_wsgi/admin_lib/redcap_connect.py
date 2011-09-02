0214a03e31c09cd3e6cee939165907e023664bca
Dan Connolly <dconnolly@kumc.edu>
2011-09-02 11:42:47 -0500

    redcap_connect.py feature test: setup survey link; update integration test
    --HG--
    branch : frontiers-layout

--- heron_wsgi/admin_lib/redcap_connect.py
+++ heron_wsgi/admin_lib/redcap_connect.py
@@ -1,12 +1,17 @@
 '''redcap_connect.py -- Connect HERON users to REDCap surveys.
 
-expects configuration a la:
+  >>> print _test_settings.inifmt('survey_xyz')
+  [survey_xyz]
+  api_url=http://redcap-host/redcap/api/
+  domain=example.edu
+  survey_id=11
+  survey_url=http://bmidev1/redcap-host/surveys/
+  token=sekret
 
-[redcap]
-TOKEN=...
-api_url=http://redcap-host/redcap/api/
-survey_url=http://bmidev1/redcap-host/surveys/
-domain=example.edu
+  >>> setup = _doctester()
+  >>> setup('john.smith@js.example', 'John Smith')
+  'http://bmidev1/redcap-host/surveys/?s=xyzpdq&user_id=john.smith%40js.example&full_name=John+Smith'
+
 '''
 
 import urllib
@@ -19,14 +24,17 @@ import config
 def survey_setup(ini, section):
     rt = config.RuntimeOptions('token api_url survey_url domain'.split())
     rt.load(ini, section)
+    return _survey_setup(rt, urllib2)
 
+
+def _survey_setup(rt, urlopener):
     def setup(userid, full_name):
         email = '%s@%s' % (userid, rt.domain)
         body = urllib.urlencode({'token': rt.token,
                                  'content': 'survey',
                                  'format': 'json',
                                  'email': email})
-        body = urllib2.urlopen(rt.api_url, body).read()
+        body = urlopener.urlopen(rt.api_url, body).read()
         surveycode = json.loads(body)['hash']
         params = urllib.urlencode({'s': surveycode,
                                    'user_id': userid,
@@ -36,13 +44,40 @@ def survey_setup(ini, section):
     return setup
 
 
-def _integration_test(ini='saa_survey.ini', section='saa_survey'):
+class _TestUrlOpener(object):
+    def urlopen(self, addr, body):
+        return _TestResponse()
+
+class _TestResponse(object):
+    def read(self):
+        return json.dumps({'PROJECT_ID': 123,
+                           'add': 0,
+                           'survey_id': _test_settings.survey_id,
+                           'hash': u'xyzpdq',
+                           'email': u'BOGUS@%s' % _test_settings.domain})
+
+_test_settings = config.TestTimeOptions(dict(
+    token='sekret',
+    api_url='http://redcap-host/redcap/api/',
+    survey_url='http://bmidev1/redcap-host/surveys/',
+    domain='example.edu',
+    survey_id=11))
+
+def _doctester():
+    return _survey_setup(_test_settings, _TestUrlOpener())
+
+
+def _integration_test(ini='integration-test.ini', section='saa_survey'):
     return survey_setup(ini, section)
 
 
 if __name__ == '__main__':
     import sys
     from pprint import pprint
-    emailAddress = sys.argv[1]
+    userid, fullName = sys.argv[1:3]
     c = _integration_test()
-    pprint(c(emailAddress))
+    try:
+        pprint(c(userid, fullName))
+    except IOError, e:
+        print e.message
+        print e
