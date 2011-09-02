04ad0bfaa4e2a05596aee31dccd33ac9bb06e8ea
Dan Connolly <dconnolly@kumc.edu>
2011-09-02 13:26:56 -0500

    oops! forgot to save config.py before commit with TestTimeOptions
    --HG--
    branch : frontiers-layout

--- heron_wsgi/admin_lib/config.py
+++ heron_wsgi/admin_lib/config.py
@@ -18,3 +18,25 @@ class RuntimeOptions(object):
             raise AttributeError, n
         return self._d.get(n, None)
 
+
+class TestTimeOptions(object):
+    '''
+      >>> tto = TestTimeOptions({'size': '10', 'color': 'blue'})
+      >>> print tto.inifmt('widget')
+      [widget]
+      color=blue
+      size=10
+
+    '''
+    def __init__(self, settings):
+        self._settings = settings
+
+    def __getattr__(self, n):
+        if n not in self._settings:
+            raise AttributeError, n
+        return self._settings[n]
+        
+    def inifmt(self, section):
+        s = self._settings
+        lines = ['%s=%s' % (k, s[k]) for k in sorted(s.keys())]
+        return '\n'.join(["[%s]" % section] + lines)
