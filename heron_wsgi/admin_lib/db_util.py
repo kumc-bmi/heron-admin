e2840e9ef466310bd6c853bd6bec6c4189bdd866
Dan Connolly <dconnolly@kumc.edu>
2011-09-03 00:15:25 -0500

    don't import cx_Oracle until we need it
    --HG--
    branch : frontiers-layout

--- heron_wsgi/admin_lib/db_util.py
+++ heron_wsgi/admin_lib/db_util.py
@@ -3,15 +3,14 @@
 
 from contextlib import contextmanager
 
-import cx_Oracle
-import MySQLdb # http://mysql-python.sourceforge.net/MySQLdb.html#mysqldb
-
 def oracle_connect(u, p, host, port, sid):
+    import cx_Oracle
     dsn = cx_Oracle.makedsn(host, port, sid)
     return cx_Oracle.connect(dsn=dsn, user=u, password=p)
 
 
 def mysql_connect(u, p, host, port, db):
+    import MySQLdb # http://mysql-python.sourceforge.net/MySQLdb.html#mysqldb
     return MySQLdb.connect(host=host, user=u, passwd=p, port=port, db=db)
 
 
@@ -24,7 +23,7 @@ def transaction(conn):
     c = conn.cursor()
     try:
         yield c
-    except cx_Oracle.DatabaseError:
+    except IOError:
         conn.rollback()
         raise
     else:
