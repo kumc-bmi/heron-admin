413cd8bb486b426520c923440dd05fe6c0cb4c96
Dan Connolly <dconnolly@kumc.edu>
2011-09-13 15:16:22 -0500

    add test for sponsorship (not integrated with web layer) (#535)

--- heron_wsgi/admin_lib/heron_policy.py
+++ heron_wsgi/admin_lib/heron_policy.py
@@ -1,12 +1,16 @@
 '''heron_policy.py -- HERON policy decisions, records
 
   >>> m = medcenter._mock()
-  >>> survey_id = 11  # arbitrary
-  >>> hp = HeronRecords(_TestDBConn(), m, _TestTimeSource(), survey_id)
+  >>> hp = HeronRecords(_TestDBConn(), m, _TestTimeSource(),
+  ...                   saa_survey_id=11, oversight_project_id=34, qty_institutions=3)
+
 
 Look up an investigator and a student::
   >>> fac = m.affiliate('john.smith')
+  >>> fac
+  John Smith <john.smith@js.example>
   >>> stu = m.affiliate('bill.student')
+  >>> stu2 = m.affiliate('some.one')
 
 See if they're qualified faculty::
   >>> hp.q_faculty(fac)
@@ -15,6 +19,16 @@ See if they're qualified faculty::
   Traceback (most recent call last):
     ...
   NotFaculty
+
+See if the students are qualified in some way::
+
+  >>> hp.q_any(stu)
+  Traceback (most recent call last):
+    ...
+  NotSponsored
+
+  >>> hp.q_any(stu2)
+  OK:Some One <some.one@js.example>
 
 Get an actual access qualification; i.e. check for
 system access agreement and human subjects training::
@@ -33,13 +47,18 @@ REDCAPDB_CONFIG_SECTION='redcapdb'
 
 class HeronRecords(object):
     # TODO: connection pooling/management?
-    def __init__(self, conn, medcenter, timesrc, saa_survey_id):
+    def __init__(self, conn, medcenter, timesrc,
+                 saa_survey_id, oversight_project_id, qty_institutions):
         self._conn = conn
         self._m = medcenter
         self._t = timesrc
         self._saa_survey_id = saa_survey_id
+        self._oversight_project_id = oversight_project_id
+        self.qty_institutions = qty_institutions
 
     def check_saa_signed(self, agent):
+        '''Test for an authenticated SAA survey response bearing the agent's email address.
+        '''
         with transaction(self._conn) as q:
             q.execute(
                 '''select p.survey_id, p.participant_email, r.response_id, r.record, r.completion_time
@@ -61,21 +80,48 @@ class HeronRecords(object):
         return OK(agent)
 
     def q_faculty(self, agent):
+        '''Test whether the medcenter considers this agent to be faculty.
+        '''
         self._m.checkFaculty(agent)
         return OK(agent)
 
     def q_sponsored(self, agent):
-        raise NotSponsored()
-        # TODO: port this to redcap
-        if not self._agent_test(
-            '''select user_id
-               from heron.SPONSORSHIP
-               where user_id=:u
-	         and (expire_date is null or expire_date>sysdate)
-                 and access_type='VIEW_ONLY' 
-                 and (kuh_approval_status ='A'
-                      and kumc_approval_status ='A'
-                      and ukp_approval_status ='A')''', 'u', agent):
+        '''Test for sponsorship approval from each participating institution.
+
+        In the oversight_project, we assume userid of sponsored users
+        are stored in REDCap fields with names like user_id_% and
+        approval decisions are stored in fields with names like
+        approve_%, with a distinct approve_% field for each
+        participating institution and coded yes=1/no=2/defer=3.
+        '''
+        with transaction(self._conn) as q:
+            q.execute('''
+select record, count(*)
+from (
+select distinct
+  candidate.record, candidate.userid, review.institution, review.decision
+from (
+  select record, value as userid
+  from redcap_data
+  where project_id=:project_id
+  and field_name like 'user_id_%'
+) as candidate
+join  (
+  select record, field_name as institution, value as decision
+  from redcap_data
+  where project_id=:project_id
+  and field_name like 'approve_%'
+) as review on review.record = candidate.record
+where decision=1 and userid=:userid
+) review
+having count(*) = :qty
+'''
+                      , dict(project_id=self._oversight_project_id,
+                             userid=agent.userid(),
+                             qty=self.qty_institutions))
+            answers = q.fetchall()
+
+        if not answers:
             raise NotSponsored()
         return OK(agent)
 
@@ -178,11 +224,26 @@ class _TestTrx():
     def execute(self, q, params=[]):
         if params == {'mail': 'john.smith@js.example', 'survey_id': 11}:
             self._results = [(11, 'john.smith@js.example', 123, 123, '2011-01-01')]
+        elif 'count' in q:  # assume it's the sponsored query
+            if 'some.one' in params.get('userid', ''):
+                self._results = [(9, 3)]
+            else:
+                self._results = []
         else:
             self._results = []
 
+        self._row = 0
+
     def fetchmany(self):
         return self._results
+
+    def fetchall(self):
+        return self._results
+
+    def fetchone(self):
+        row = self._results[self._row]
+        self._row += 1
+        return row
 
     def close(self):
         pass
