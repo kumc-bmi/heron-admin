cd73712e918cf410b5ed1a58e5999bab9d9cb69a
Dan Connolly <dconnolly@kumc.edu>
2011-12-12 17:27:40 -0600

    - working on logging (#760) - pep8 line lengths and such

--- heron_wsgi/admin_lib/heron_policy.py
+++ heron_wsgi/admin_lib/heron_policy.py
@@ -24,16 +24,25 @@ See if they're qualified faculty::
   >>> facreq.faculty
   Faculty(John Smith <john.smith@js.example>)
 
-  >>> facreq.user.ensure_saa_survey()
-  'http://bmidev1/redcap-host/surveys/?s=8074&full_name=Smith%2C+John&user_id=john.smith'
+  >>> facreq.user.ensure_saa_survey().split('?')
+  ... # doctest: +NORMALIZE_WHITESPACE
+  ['http://bmidev1/redcap-host/surveys/',
+   's=8074&full_name=Smith%2C+John&user_id=john.smith']
 
   >>> hp.issue(stureq)
   [Affiliate(Bill Student <bill.student@js.example>)]
   >>> stureq.faculty is None
   True
-  >>> facreq.faculty.ensure_oversight_survey(['some.one'],
-  ...                                        what_for=HeronRecords.DATA_USE)
-  'http://bmidev1/redcap-host/surveys/?s=8074&full_name=Smith%2C+John&multi=yes&name_etc_1=One%2C+Some%0A%0A&user_id=john.smith&user_id_1=some.one&what_for=2'
+  >>> facreq.faculty.ensure_oversight_survey(
+  ...        ['some.one'], what_for=HeronRecords.DATA_USE).split('&')
+  ... # doctest: +NORMALIZE_WHITESPACE
+  ['http://bmidev1/redcap-host/surveys/?s=8074',
+   'full_name=Smith%2C+John',
+   'multi=yes',
+   'name_etc_1=One%2C+Some%0A%0A',
+   'user_id=john.smith',
+   'user_id_1=some.one',
+   'what_for=2']
 
 See if the students are qualified in some way::
 
@@ -166,10 +175,10 @@ import noticelog
 import disclaimer
 from disclaimer import Disclaimer, Acknowledgement, KTimeSource
 
-SAA_CONFIG_SECTION='saa_survey'
-OVERSIGHT_CONFIG_SECTION='oversight_survey'
-PERM_USER=__name__ + '.user'
-PERM_FACULTY=__name__ + '.faculty'
+SAA_CONFIG_SECTION = 'saa_survey'
+OVERSIGHT_CONFIG_SECTION = 'oversight_survey'
+PERM_USER = __name__ + '.user'
+PERM_FACULTY = __name__ + '.faculty'
 
 log = logging.getLogger(__name__)
 
@@ -212,7 +221,8 @@ class HeronRecords(object):
             pm=i2b2pm.I2B2PM,
             saa_opts=(rtconfig.Options, SAA_CONFIG_SECTION),
             oversight_opts=(rtconfig.Options, OVERSIGHT_CONFIG_SECTION),
-            engine=(sqlalchemy.engine.base.Connectable, redcapdb.CONFIG_SECTION),
+            engine=(sqlalchemy.engine.base.Connectable,
+                    redcapdb.CONFIG_SECTION),
             smaker=(sqlalchemy.orm.session.Session, redcapdb.CONFIG_SECTION),
             timesrc=KTimeSource,
             urlopener=urllib.URLopener)
@@ -256,6 +266,7 @@ class HeronRecords(object):
             '''
             def lookup(self, uid):
                 return mc.lookup(uid)
+
             def search(self, max, cn, sn, givenname):
                 return mc.search(max, cn, sn, givenname)
 
@@ -264,7 +275,7 @@ class HeronRecords(object):
                 return hr._saa_rc(badge.cn, params)
 
             def get_sig(self):
-                return hr._check_saa_signed(badge.mail)  #@@seal date
+                return hr._check_saa_signed(badge.mail)  # @@seal date
 
             def ensure_oversight(self, params):
                 return hr._oversight_rc(badge.cn, params, multi=True)
@@ -273,6 +284,8 @@ class HeronRecords(object):
                 try:
                     when = mc.training(req.idvault_entry)
                 except (KeyError):
+                    log.info('no training on file for: %s (%s)',
+                             badge.cn, badge.full_name())
                     raise NoTraining
                 except (IOError):
                     log.warn('failed to look up training due to IOError')
@@ -280,11 +293,13 @@ class HeronRecords(object):
                     raise NoTraining
                 current = when >= hr._t.today().isoformat()
                 if not current:
+                    log.info('training expried %s for: %s (%s)',
+                             when, badge.cn, badge.full_name())
                     raise NoTraining(when)
                 return when
 
             def get_sponsor(self):
-                return hr._sponsored(badge.cn)  #@@ seal sponsor uid
+                return hr._sponsored(badge.cn)  # @@ seal sponsor uid
 
             def repository_account(self, user, sponsor, sig, training):
                 #@@ todo: check user, sponsor, sig, training?
@@ -292,8 +307,7 @@ class HeronRecords(object):
 
         ex = fac = user = None
         req.disclaimer, ack = self._disclaimer_acknowledgement(badge.cn)
-        log.debug('disclaimer: %s', req.disclaimer)
-        log.debug('ack: %s', ack)
+
         if badge.cn in self._executives:
             ex = Executive(badge,
                            req.idvault_entry,
@@ -306,44 +320,52 @@ class HeronRecords(object):
                                Record(), Browser(), ack)
                 user = fac
             except medcenter.NotFaculty:
-                user = Affiliate(badge, req.idvault_entry, Record(), Browser(), ack)
+                user = Affiliate(badge, req.idvault_entry,
+                                 Record(), Browser(), ack)
 
         req.executive = ex
         req.faculty = fac
         req.user = user
 
+        log.info('HeronRecords.issue executive: %s faculty: %s user: %s',
+                 ex, fac, user)
+
         return [user]
 
     def audit(self, cap, p=PERM_USER):
-        log.info('HeronRecords.audit(%s, %s)' % (cap, p))
+        log.debug('HeronRecords.audit(%s, %s)' % (cap, p))
         if not isinstance(cap, Faculty if p is PERM_FACULTY else Affiliate):
             raise TypeError
         self._mc.read_badge(cap.idcap)
 
     def _disclaimer_acknowledgement(self, user_id):
         '''
-        @returns: (current disclaimer, acknowledgement of user_id); ack is None if not found.
+        @returns: (current disclaimer, acknowledgement of user_id);
+                  ack is None if not found.
         '''
         s = self._smaker()
 
-        d = s.query(Disclaimer).filter(Disclaimer.current==1).first()
+        d = s.query(Disclaimer).filter(Disclaimer.current == 1).first()
 
         if d:
-            log.debug('current disclaimer address: %s', d.url)
-            a = s.query(Acknowledgement \
-                            ).filter(Acknowledgement.disclaimer_address==d.url
-                                     ).filter(Acknowledgement.user_id==user_id).first()
+            log.debug('disclaimer: %s', d)
+            a = s.query(Acknowledgement).\
+                filter(Acknowledgement.disclaimer_address == d.url).\
+                filter(Acknowledgement.user_id == user_id).first()
         else:
             log.warn('no current disclaimer!')
             log.debug('session engine: %s', s.bind)
             a = None
 
+        log.info('disclaimer ack: %s', a)
         return d, a
 
     def _check_saa_signed(self, mail):
         '''Test for an authenticated SAA survey response.
         '''
-        if not self._engine.execute(_saa_query(mail, self._saa_survey_id)).fetchall():
+        if not self._engine.execute(_saa_query(mail, self._saa_survey_id)).\
+                fetchall():
+            log.info('no SAA: %s', mail)
             raise NoAgreement()
 
     def _sponsored(self, uid):
@@ -352,31 +374,37 @@ class HeronRecords(object):
         # mysql work-around for
         # 1248, 'Every derived table must have its own alias'
         dc = dc.alias('mw')
-        # perhaps we should use count and scalar here?
         q = dc.select(and_(dc.c.candidate == uid,
                            dc.c.decision == DecisionRecords.YES))
 
         for ans in self._engine.execute(q).fetchall():
             if ans.dt_exp <= '' or self._t.today().isoformat() <= ans.dt_exp:
+                log.info('sponsorship OK: %s', ans)
                 return True
 
+        log.info('not sponsored: %s', uid)
         raise NotSponsored()
-
 
 
 def _saa_query(mail, survey_id):
     '''
       >>> q = _saa_query('john.smith@js.example', 11)
       >>> print str(q)
-      SELECT r.response_id, r.participant_id, r.record, r.first_submit_time, r.completion_time, r.return_code, p.participant_id, p.survey_id, p.arm_id, p.hash, p.legacy_hash, p.participant_email, p.participant_identifier 
-      FROM redcap_surveys_response AS r JOIN redcap_surveys_participants AS p ON r.participant_id = p.participant_id 
-      WHERE p.participant_email = :participant_email_1 AND p.survey_id = :survey_id_1
+      ... # doctest: +NORMALIZE_WHITESPACE
+      SELECT r.response_id, r.participant_id, r.record,
+      r.first_submit_time, r.completion_time, r.return_code,
+      p.participant_id, p.survey_id, p.arm_id, p.hash, p.legacy_hash,
+      p.participant_email, p.participant_identifier FROM
+      redcap_surveys_response AS r JOIN redcap_surveys_participants AS
+      p ON r.participant_id = p.participant_id WHERE
+      p.participant_email = :participant_email_1 AND p.survey_id =
+      :survey_id_1
 
     '''
     r = redcapdb.redcap_surveys_response.alias('r')
     p = redcapdb.redcap_surveys_participants.alias('p')
     return r.join(p, r.c.participant_id == p.c.participant_id).select().where(
-            and_(p.c.participant_email==mail, p.c.survey_id==survey_id))
+            and_(p.c.participant_email == mail, p.c.survey_id == survey_id))
 
 
 def _sponsor_queries(oversight_project_id):
@@ -410,7 +438,7 @@ def _sponsor_queries(oversight_project_id):
       SELECT cd_record AS record,
              cd_decision AS decision,
              who_userid AS candidate,
-             expire_dt_exp AS dt_exp 
+             expire_dt_exp AS dt_exp
       FROM
         (SELECT cd.project_id AS cd_project_id,
                 cd.record AS cd_record,
@@ -425,25 +453,25 @@ def _sponsor_queries(oversight_project_id):
          FROM
            (SELECT redcap_data.project_id AS project_id,
                    redcap_data.record AS record,
-                   redcap_data.value AS decision, count(*) AS count_1 
-            FROM redcap_data 
+                   redcap_data.value AS decision, count(*) AS count_1
+            FROM redcap_data
             WHERE redcap_data.field_name LIKE :field_name_1
               AND redcap_data.project_id = :project_id_1
             GROUP BY redcap_data.project_id, redcap_data.record,
-                     redcap_data.value 
+                     redcap_data.value
             HAVING count(*) = :count_2) AS cd
            JOIN
              (SELECT redcap_data.project_id AS project_id,
                      redcap_data.record AS record,
-                     redcap_data.value AS userid 
-              FROM redcap_data 
+                     redcap_data.value AS userid
+              FROM redcap_data
               WHERE redcap_data.field_name LIKE :field_name_2) AS who
            ON who.record = cd.record
            AND who.project_id = cd.project_id
            LEFT OUTER JOIN (SELECT redcap_data.project_id AS project_id,
                                    redcap_data.record AS record,
-                                   redcap_data.value AS dt_exp 
-                            FROM redcap_data 
+                                   redcap_data.value AS dt_exp
+                            FROM redcap_data
                             WHERE redcap_data.field_name = :field_name_3)
                              AS expire
            ON expire.record = cd.record AND expire.project_id = cd.project_id)
@@ -467,11 +495,11 @@ def _sponsor_queries(oversight_project_id):
                        rd.c.value.label('decision'),
                        func.count())).where(
         and_(rd.c.field_name.like('approve_%'),
-             rd.c.project_id==oversight_project_id)
-        ).group_by(rd.c.project_id,
-                   rd.c.record,
-                   rd.c.value).having(
-            func.count() == len(HeronRecords.institutions)).alias('cd')
+             rd.c.project_id == oversight_project_id)).\
+             group_by(rd.c.project_id,
+                      rd.c.record,
+                      rd.c.value).having(
+                 func.count() == len(HeronRecords.institutions)).alias('cd')
 
     # todo: consider combining record, event, project_id into one attr
     candidate = select((rd.c.project_id, rd.c.record,
@@ -484,12 +512,12 @@ def _sponsor_queries(oversight_project_id):
 
     j = decision.join(candidate,
                       and_(candidate.c.record == decision.c.record,
-                           candidate.c.project_id == decision.c.project_id)
-                      ).outerjoin(dt_exp, and_(
+                           candidate.c.project_id == decision.c.project_id)).\
+                           outerjoin(dt_exp, and_(
             dt_exp.c.record == decision.c.record,
-            dt_exp.c.project_id == decision.c.project_id)
-                                  ).alias('cdwho').select()
-        
+            dt_exp.c.project_id == decision.c.project_id)).\
+            alias('cdwho').select()
+
     cdwho = j.with_only_columns((j.c.cd_record.label('record'),
                                  j.c.cd_decision.label('decision'),
                                  j.c.who_userid.label('candidate'),
@@ -519,7 +547,6 @@ class NoAgreement(NoPermission):
     pass
 
 
-
 class Affiliate(object):
     def __init__(self, badge, idcap, record, browser, ack):
         self.badge = badge
@@ -534,7 +561,7 @@ class Affiliate(object):
     def sort_name(self):
         # law of demeter says move this to Badge()...
         return "%s, %s" % (self.badge.sn, self.badge.givenname)
-        
+
     def ensure_saa_survey(self):
         return self.record.ensure_saa(dict(user_id=self.badge.cn,
                                            full_name=self.sort_name()))
@@ -554,6 +581,7 @@ class Affiliate(object):
                                               self.signature(),
                                               self.training())
 
+
 class Executive(Affiliate):
     def __repr__(self):
         return 'Executive(%s)' % (self.badge)
@@ -561,7 +589,7 @@ class Executive(Affiliate):
     def sponsor(self):
         return self
 
-    
+
 class Faculty(Affiliate):
     def __repr__(self):
         return 'Faculty(%s)' % (self.badge)
@@ -580,6 +608,7 @@ class Faculty(Affiliate):
                                                  what_for=what_for,
                                                  multi='yes'))
 
+
 def team_params(lookup, uids):
     r'''
     >>> import pprint
@@ -592,14 +621,13 @@ def team_params(lookup, uids):
      ('name_etc_2', 'Student, Bill\n\n')]
 
     '''
-    nested = [[('user_id_%d' % (i+1), uid),
-               ('name_etc_%d' % (i+1), '%s, %s\n%s\n%s' % (
+    nested = [[('user_id_%d' % (i + 1), uid),
+               ('name_etc_%d' % (i + 1), '%s, %s\n%s\n%s' % (
                     a.sn, a.givenname, a.title, a.ou))]
-              for (i, uid, a) in 
+              for (i, uid, a) in
               [(i, uids[i], lookup(uids[i]))
                for i in range(0, len(uids))]]
     return itertools.chain.from_iterable(nested)
-
 
 
 class DecisionRecords(object):
@@ -660,8 +688,8 @@ class DecisionRecords(object):
 
 class _DataDict(object):
     def __init__(self, name,
-                 base = os.path.join(os.path.dirname(__file__),
-                                     '..', 'redcap_dd')):
+                 base=os.path.join(os.path.dirname(__file__),
+                                   '..', 'redcap_dd')):
         def open_it():
             return open(os.path.join(base, name + '.csv'))
         self._open = open_it
@@ -685,7 +713,8 @@ class _DataDict(object):
 class TestSetUp(disclaimer.TestSetUp):
     @singleton
     @provides((sqlalchemy.orm.session.Session, redcapdb.CONFIG_SECTION))
-    @inject(engine=(sqlalchemy.engine.base.Connectable, redcapdb.CONFIG_SECTION),
+    @inject(engine=(sqlalchemy.engine.base.Connectable,
+                    redcapdb.CONFIG_SECTION),
             timesrc=KTimeSource,
             srt=(rtconfig.Options, SAA_CONFIG_SECTION),
             ort=(rtconfig.Options, OVERSIGHT_CONFIG_SECTION))
@@ -716,7 +745,6 @@ class TestSetUp(disclaimer.TestSetUp):
                               [(org, DecisionRecords.YES)
                                for org in HeronRecords.institutions])
 
-
         # A request to sponsor bill.student is only reviewed by 2 of 3 orgs:
         add_oversight_request('john.smith', 'John Smith', 'Cure Hair Loss',
                               ((1, 'bill.student'),),
@@ -736,7 +764,6 @@ class TestSetUp(disclaimer.TestSetUp):
                                for org in HeronRecords.institutions],
                               '1950-02-27')
 
-
         log.debug('add SAA records')
         redcapdb.redcap_surveys_participants.create(s.bind)
         s.commit()
@@ -749,7 +776,8 @@ class TestSetUp(disclaimer.TestSetUp):
                     survey_id=srt.survey_id, participant_email=email))
             s.execute(redcapdb.redcap_surveys_response.insert().values(
                     response_id=abs(hash(email)), record=abs(hash(email)),
-                    completion_time=timesrc.today() + datetime.timedelta(days=-7),
+                    completion_time=timesrc.today() + \
+                        datetime.timedelta(days=-7),
                     participant_id=abs(hash(email))))
 
         s.commit()
@@ -762,8 +790,6 @@ def add_test_eav(s, project_id, event_id, e, avs):
         s.execute(redcapdb.redcap_data.insert().values(
                 project_id=project_id, event_id=event_id,
                 record=e, field_name=a, value=v))
-
-
 
 
 class Mock(injector.Module, rtconfig.MockMixin):
@@ -825,7 +851,7 @@ if __name__ == '__main__':  # pragma nocover
     userid = sys.argv[1]
     req = medcenter.Mock.login_info(userid)
     hr, ds = RunTime.make(None, [HeronRecords, DecisionRecords])
-    hr._mc.issue(req) # umm... peeking
+    hr._mc.issue(req)  # umm... peeking
     hr.issue(req)
     print req.user.repository_account()
 
