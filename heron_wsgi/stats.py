299f03d0adfa2f0160eab7607296fad10d5ea9d6
Dan Connolly <dconnolly@kumc.edu>
2012-05-09 18:05:44 -0500

    performance report showing machine load (oops: middle tier load not so useful)   - toward more secure flow of authority with powerbox.py   - use d3.js for charting
    --HG--
    branch : audit_600

--- heron_wsgi/stats.py
+++ heron_wsgi/stats.py
@@ -9,21 +9,41 @@ from admin_lib import heron_policy
 CONFIG_SECTION = 'i2b2pm'
 
 
-class Report(object):
-    @inject(datasrc=(orm.session.Session, CONFIG_SECTION))
-    def __init__(self, datasrc):
-        self._datasrc = datasrc
+class Machine(object):
+    '''Machine Interface
 
-    def configure(self, config, route_name):
+    See :class:`heron_wsgi.powerbox.LinuxMachine` for implementation.
+    '''
+    def load(self):
+        '''Report load average(s) on the machine
+
+        :returntype: list of (minutes, load) tuples.
+        '''
+        return [(1, 0.00), (5, 0.00), (15, 0.00)]
+
+
+class Report(object):
+    @inject(datasrc=(orm.session.Session, CONFIG_SECTION),
+            machine=Machine)
+    def __init__(self, datasrc, machine):
+        self._datasrc = datasrc
+        self._machine = machine
+
+    def configure(self, config, usage_route, performance_route):
         '''Connect this view to the rest of the application
 
         :param config: a pyramid config thingy@@@
         :param route_name: a pyramid route name@@@ cite refs
 
         .. todo:: consider requiring DROC permissions on show_report
+        .. todo:: consider allowing anonymous access to performance report.
         '''
-        config.add_view(self.show_usage_report, route_name=route_name,
+        config.add_view(self.show_usage_report, route_name=usage_route,
                         request_method='GET', renderer='report1.html',
+                        permission=heron_policy.PERM_USER)
+
+        config.add_view(self.show_performance, route_name=performance_route,
+                        request_method='GET', renderer='performance.html',
                         permission=heron_policy.PERM_USER)
 
     def show_usage_report(self, res, req):
@@ -49,3 +69,6 @@ class Report(object):
 
         return dict(total_number_of_queries=count,
                     query_volume=query_volume)
+
+    def show_performance(self, res, req):
+        return dict(machine_load=self._machine.load())
