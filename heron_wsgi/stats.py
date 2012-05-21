2d12ba1fee4d37589b23752097c7ad2338b81977
Dan Connolly <dconnolly@kumc.edu>
2012-05-21 17:20:32 -0500

    more readable usage reports (#600)   - query volume by weeks/months/quarter/year/all   - include full_name in reports   - summary/detail for small result set report
    --HG--
    branch : audit_600

--- heron_wsgi/stats.py
+++ heron_wsgi/stats.py
@@ -3,6 +3,8 @@
 '''
 
 import logging
+import itertools
+import operator
 
 from injector import inject
 from sqlalchemy import orm, func, between, case
@@ -38,37 +40,74 @@ class UsageReports(object):
     def query_volume(self, recent=False):
         '''overall query volume by user
         '''
-        w = 'where qqm.create_date > sysdate - 15' if recent else ''
         return self.q('''
-            select user_id as users, count(*) as number_of_queries
-            from BLUEHERONDATA.qt_query_master qqm
-            %(WHERE)s
-            group by user_id
-            order by number_of_queries desc''' %
-                      dict(WHERE=w))
+select pud.full_name, all_time.user_id
+     , two_weeks.qty two_weeks
+     , last_month.qty last_month
+     , last_quarter.qty last_quarter
+     , last_year.qty last_year
+     , all_time.qty all_time from
+(select qqm.user_id, count(*) as qty
+from BLUEHERONDATA.qt_query_master qqm
+group by qqm.user_id) all_time
 
-    def usage_monthly(self):
-        '''frequency of usuage on a monthly basis
-        '''
-        return self.q('''
-        select user_id, to_char(qqm.create_date, 'YYYY-MM') as yearmonth
-             , count(*) as number_of_queries
-        from BLUEHERONDATA.qt_query_master qqm
-        group by to_char(qqm.create_date, 'YYYY-MM'), user_id
-        order by user_id, yearmonth
-        ''')
+left join
 
-    def patient_set_users(self, recent=False, small=False):
-        '''All users who built queries that returned set sizes less than 10
+(select qqm.user_id, count(*) as qty
+from BLUEHERONDATA.qt_query_master qqm
+where qqm.create_date >= sysdate - 14
+group by qqm.user_id) two_weeks
+
+on two_weeks.user_id = all_time.user_id
+
+left join
+
+(select qqm.user_id, count(*) as qty
+from BLUEHERONDATA.qt_query_master qqm
+where qqm.create_date >= sysdate - 30
+group by qqm.user_id) last_month
+
+on last_month.user_id = all_time.user_id
+
+left join
+
+(select qqm.user_id, count(*) as qty
+from BLUEHERONDATA.qt_query_master qqm
+where qqm.create_date >= sysdate - 90
+group by qqm.user_id) last_quarter
+
+on last_quarter.user_id = all_time.user_id
+
+left join
+
+(select qqm.user_id, count(*) as qty
+from BLUEHERONDATA.qt_query_master qqm
+where qqm.create_date >= sysdate - 365
+group by qqm.user_id) last_year
+
+on last_year.user_id = all_time.user_id
+
+        join i2b2pm.pm_user_data pud
+          on all_time.user_id = pud.user_id
+
+order by nvl(two_weeks.qty, -1) desc, nvl(all_time.qty, -1) desc
+                      ''')
+
+    def patient_set_queries(self, recent=False, small=False):
+        '''Queries that returned set sizes less than 10
         '''
-        r = ('and qqm.create_date > sysdate - 15'
+        r = ('and qqm.create_date > sysdate - 45'
              if recent else '')
         s = ('and qqri.real_set_size < 11 and qqri.set_size > 0'
              if small else '')
 
         return self.q('''
-        select qqm.user_id, qqm.name, qqm.create_date, qqri.set_size
+        select pud.full_name, qqm.user_id
+             , qqm.query_master_id, qqm.name, qqm.create_date
+             , qqri.set_size
         from BLUEHERONDATA.qt_query_master qqm
+        join i2b2pm.pm_user_data pud
+          on pud.user_id = qqm.user_id
         join BLUEHERONDATA.qt_query_instance qqi
           on qqm.query_master_id=qqi.query_master_id
         join BLUEHERONDATA.qt_query_result_instance qqri
@@ -77,7 +116,7 @@ class UsageReports(object):
           on qqri.result_type_id=qqrt.result_type_id
         where qqri.result_type_id=1
         %(RECENT)s %(SMALL)s
-        order by create_date desc
+        order by substr(qqm.user_id, 2), create_date desc
         ''' % dict(RECENT=r, SMALL=s))
 
     def small_set_concepts(self):
@@ -85,6 +124,7 @@ class UsageReports(object):
         than 10.'''
         return self.q('''
         select user_id
+             , qm.query_master_id
              , create_date
              , name as query_name
              , substr(extract(column_value, '/item/item_name/text()'), 1)
@@ -109,7 +149,7 @@ class UsageReports(object):
                         on qqri.result_type_id=qqrt.result_type_id
                       where qqri.real_set_size < 11 and qqri.set_size > 0
                         and qqri.result_type_id=1
-                        and qqm.create_date > sysdate - 15)
+                        and qqm.create_date > sysdate - 45)
         order by create_date desc
         ''')
 
@@ -131,16 +171,13 @@ class UsageReports(object):
 
     def show_usage_report(self, res, req):
         return dict(total_number_of_queries=self.total_number_of_queries(),
-                    query_volume=self.query_volume(),
-                    query_volume_recent=self.query_volume(recent=True),
-                    usage_monthly=self.usage_monthly())
+                    query_volume=self.query_volume())
 
     def show_small_set_report(self, res, req):
         return dict(
-            recent_small=self.patient_set_users(recent=True, small=True),
-            small=self.patient_set_users(recent=False, small=True),
-            all=self.patient_set_users(recent=False, small=False),
-            small_set_concepts=self.small_set_concepts())
+            summary=self.patient_set_queries(recent=True, small=True),
+            detail=itertools.groupby(self.small_set_concepts(),
+                                     operator.itemgetter('query_master_id')))
 
 
 class Machine(object):
@@ -350,7 +387,8 @@ def _json_val(x):
         return [getattr(x, f) for f in ('year', 'month', 'day')]
     elif t is datetime.datetime:
         return [getattr(x, f) for f in ('year', 'month', 'day',
-                                        'hour', 'minute', 'second', 'microsecond')]
+                                        'hour', 'minute', 'second',
+                                        'microsecond')]
     elif t is datetime.timedelta:
         return x.microseconds / 1000 + (1000 * x.seconds)
     elif t is decimal.Decimal:
