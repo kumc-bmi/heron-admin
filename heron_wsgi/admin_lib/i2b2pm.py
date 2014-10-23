f25864c1540f5c62cbdb1605d5ae06d1c2efa75e
Nathan Graham <ngraham@kumc.edu>
2014-10-23 17:11:14 -0500

    Update metadata jndi names to jboss 7 and ironjacamar style.
    --HG--
    branch : i2b2_1_7_2944

--- heron_wsgi/admin_lib/i2b2pm.py
+++ heron_wsgi/admin_lib/i2b2pm.py
@@ -421,7 +421,7 @@ class Project(Base, Audited):
 
 
 class RunTime(rtconfig.IniModule):  # pragma: nocover
-    jndi_name = 'PMBootStrapDS'
+    jndi_name = 'java:/PMBootStrapDS'
 
     # abusing Session a bit; this really provides a subclass, not an
     # instance, of Session
