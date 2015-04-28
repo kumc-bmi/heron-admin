ba6752332357d72dde8bde2421bfc2981f99f10e
Dan Connolly <dconnolly@kumc.edu>
2015-04-27 19:16:49 -0500

    year_after() in sqlite, mysql
    --HG--
    branch : citi_2760

--- heron_wsgi/traincheck/sqlaview.py
+++ heron_wsgi/traincheck/sqlaview.py
@@ -2,10 +2,11 @@
 
 '''
 
+from sqlalchemy import DateTime
 from sqlalchemy.schema import DDLElement
 from sqlalchemy.ext.compiler import compiles
 from sqlalchemy.sql.expression import (
-    Executable, ClauseElement)
+    FunctionElement, Executable, ClauseElement)
 
 
 class CreateView(Executable, ClauseElement):
@@ -54,3 +55,25 @@ def compile3(element, compiler, **kw):
     pfx, sep = ((compiler.preparer.quote(element.schema, ""), '.')
                 if element.schema else ('', ''))
     return "DROP VIEW IF EXISTS %s%s%s" % (pfx, sep, element.name)
+
+
+class year_after(FunctionElement):
+    type = DateTime()
+    name = 'year_after'
+
+
+@compiles(year_after)
+def year_after_default(element, compiler, **kw):
+    return compiler.visit_function(element)
+
+
+@compiles(year_after, 'sqlite')
+def year_after_sqlite(element, compiler, **kw):
+    [arg1] = list(element.clauses)
+    return "datetime(%s, '+1 year')" % compiler.process(arg1)
+
+
+@compiles(year_after, 'mysql')
+def year_after_mysql(element, compiler, **kw):
+    [arg1] = list(element.clauses)
+    return 'adddate(%s, interval 1 year)' % compiler.process(arg1)
