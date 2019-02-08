041b0157c06e5df69cd7a9fe66606a88486a57f0
Dan Connolly <dconnolly@kumc.edu>
2019-02-08 09:31:53 -0600

    traincheck: log entire record

--- heron_wsgi/traincheck/traincheck.py
+++ heron_wsgi/traincheck/traincheck.py
@@ -321,8 +321,8 @@ class CRS(TableDesign):
         """
         if not (record.StudentID is None or
                 record.StudentID.strip().isdigit()):
-            log.warning('%s %s: expected digits for StudentID: %s',
-                        record.dtePassed, record.memberEmail, record.StudentID)
+            log.warning('StudentID: expected digits: %s\n%s',
+                        record.StudentID, record)
             return False
         return True
 
