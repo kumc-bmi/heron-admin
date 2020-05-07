32104a64289dfa45906f30fe011b2b1deae5ad05
Nathan Hensel <nathanihensel@gmail.com>
2020-05-07 12:41:48 -0500

    dont disable redcap roles for ADMINs

--- heron_wsgi/admin_lib/i2b2pm.py
+++ heron_wsgi/admin_lib/i2b2pm.py
@@ -157,18 +157,26 @@ The ADMIN role is not project specific:
   >>> sorted(set([role.project_id for role in js.roles]))
   ['@', u'BlueHeron', u'REDCap_4']
 
-If he has the ADMIN role for a given project, other roles should be left static.
+If he has the ADMIN role for a given project, blueheron roles only should be
+left static.
   >>> s = dbsrc()
   >>> admin_role = UserRole(user_id='john.smith', project_id='BlueHeron',
   ...     user_role_cd='ADMIN', status_cd='A')
   >>> s.add(admin_role)
   >>> auth, js3 = pm.authz('john.smith', 'John Smith', 'BlueHeron')
-  >>> obfsc_role = s.query(UserRole).filter_by(user_id = 'john.smith', user_role_cd = 'DATA_OBFSC', project_id='BlueHeron').one()
+  >>> obfsc_role = s.query(UserRole).filter_by(user_id = 'john.smith',
+  ...                                          user_role_cd = 'DATA_OBFSC',
+  ...                                          project_id='BlueHeron').one()
   >>> s.delete(obfsc_role)
   >>> auth, js3 = pm.authz('john.smith', 'John Smith', 'BlueHeron')
-  >>> js = s.query(UserRole).filter_by(user_id = 'john.smith', project_id='BlueHeron').all()
+  >>> js = s.query(UserRole).filter_by(user_id = 'john.smith',
+  ...                                  project_id='BlueHeron').all()
   >>> sorted(set([role.user_role_cd for role in js]))
   ['ADMIN', 'DATA_AGG', 'DATA_DEID', 'DATA_LDS', 'USER']
+  >>> auth, js3 = pm.authz('john.smith', 'John Smith', 'REDCap_4')
+  >>> js = s.query(User).filter_by(user_id = 'john.smith').one()
+  >>> sorted(set([role.user_role_cd for role in js.roles]))
+  ['ADMIN', 'DATA_AGG', 'DATA_DEID', 'DATA_LDS', 'DATA_OBFSC', 'USER']
 
 """
 
@@ -326,6 +334,20 @@ class I2B2PM(ocap_file.Token):
                                       change_date=t, status_cd='A')
                     log.info('I2B2PM: adding: %s', myrole)
                     me.roles.append(myrole)
+        else:
+            ds.query(UserRole).filter(and_(
+                UserRole.user_id == uid,
+                UserRole.project_id.like('REDCap_%'))).\
+                delete(synchronize_session='fetch')
+
+            for project in set([project_id, DEFAULT_PID]):
+                if "REDCap_" in project:
+                    for r in roles:
+                        myrole = UserRole(user_id=uid, project_id=project,
+                                          user_role_cd=r, entry_date=t,
+                                          change_date=t, status_cd='A')
+                        log.info('I2B2PM: adding REDCap role: %s', myrole)
+                        me.roles.append(myrole)
 
         ds.commit()
         return auth, me
