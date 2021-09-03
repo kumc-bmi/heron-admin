7f32903199699cb8fe822b4d11bfa00a3ce1309b
kanuforo <anuforok@gmail.com>
2021-09-02 19:30:42 -0500

    revert to initial logic

--- heron_wsgi/traincheck/traincheck.py
+++ heron_wsgi/traincheck/traincheck.py
@@ -461,16 +461,16 @@ def TrainingRecordsRd(acct):
     return [__getitem__], dict(lookup_query=lookup)
 
 
-courses = ['CITI Biomedical Researchers', 'CITI Social Behavioral Researchers',
-'CITI Human Subjects Research', 'CITI Biomedical Researchers Refresher Course',
-'Human Research Biomedical Research Refresher Course',
-'CITI Biomedical Researchers;CITI Biomedical Researchers;2 - Refresher Course',
-'CITI  Human Subjects Research / Social Behavioral Researchers / Refresher Course']
-
-
 @maker
 def TrainingRecordsAdmin(acct, exempt_pid,
-                         course_groups=courses,
+                         course_groups=[
+                             'CITI Biomedical Researchers',
+                             'CITI Social Behavioral Researchers',
+                             'CITI Human Subjects Research',
+                             'CITI Biomedical Researchers Refresher Course',
+                             'Human Research Biomedical Research Refresher Course',
+                             'CITI Biomedical Researchers;CITI Biomedical Researchers;2 - Refresher Course',
+                             'CITI  Human Subjects Research / Social Behavioral Researchers / Refresher Course'],
                          years=3, basis=-6):
     '''Administrative access to training records
 
