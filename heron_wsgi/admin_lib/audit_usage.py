fd636cf6e99b192c582ca3d9e8739c8a7c821a49
Lav Patel <lavpatel39@gmail.com>
2023-04-12 13:09:04 -0500

    disabled debugging on dev

--- heron_wsgi/admin_lib/audit_usage.py
+++ heron_wsgi/admin_lib/audit_usage.py
@@ -193,7 +193,7 @@ substr(qm.request_xml
 
  from %(crc)s.qt_pdo_query_master qm
 join %(pm)s.pm_user_data ud on qm.user_id=ud.user_id
-where qm.create_date>current_date-99914
+where qm.create_date>current_date-14
 ) rqp order by rqp.create_date desc)rqp
 
 order by rqp.create_date desc
